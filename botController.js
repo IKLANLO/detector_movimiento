35b5a808d4737781df0d5efb6416e1deb1ab6f96
IKLANLO <ilandaberea@yahoo.es>
2026-06-18 20:16:15 +0200

    cambiado grupo receptor

--- botController.js
+++ botController.js
@@ -6,7 +6,7 @@ import { spawn } from 'child_process'
 import { startStream } from './streamController.js'
 
 const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN
-const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID_PRUEBA
+const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID
 
 export async function sendMessage(message, imagePath) {
     try {
